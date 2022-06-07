57721a59347f1e854794f0861313ae07d4c8e03b
richtepa <paul@richter.dev>
2022-06-07 17:05:37 +0200

    load all maps before starting

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+mapNames = ["austria", "italia_1"];
 longest = 0;
 fastest = Infinity;
 
@@ -7,28 +8,33 @@ window.onload = function () {
 
 async function load() {
     visu = new Visu(document.getElementById("content"));
-    map = new Map();
-    //await map.load("austria", document.getElementById("map"));
-    await map.load("italia_1", document.getElementById("map"));
-    visu.setMap(map);
+
+    maps = new Object();
+    for (mapName of mapNames) {
+        var map = new Map();
+        await map.load(mapName, document.getElementById("map"));
+        maps[mapName] = map;
+    }
+
+    visu.setMap(maps["italia_1"]);
     visu.load();
-    
+
     //var bestJSON = await loadJson("best-austria.json");
     var bestJSON = await loadJson("best-italia_1.json");
     visu.nnCoordinator.nextNNs[1] = visu.nnCoordinator.createNNfromJSON(bestJSON);
-    
+
     start();
 }
 
 function start() {
     running = true;
     loop = window.setInterval(function () {
-        for(car of visu.cars){
+        for (car of visu.cars) {
             car.frame();
         }
         visu.frame();
     }, 1000 / visu.fps);
-    
+
     visu.clickPoint = false;
     console.log(JSON.stringify(visu.clickPoints));
 }
@@ -40,8 +46,8 @@ function stop() {
     visu.clickPoint = true;
 }
 
-function pause(){
-    if(running){
+function pause() {
+    if (running) {
         stop();
     } else {
         start();
